5fd681644235b4b14920a1a13cab186ef1395f38
Muirey03 <tcmuir03@gmail.com>
2022-03-14 15:59:52 +0000

    Perspective projection instead of orthographic projection (nearly)

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,27 @@
+import { Vector } from './vector.js';
+
 export class Camera {
-	constructor (pos, dir) {
+	constructor (pos, dir /* TODO: replace with xRot, yRot */, FOV) {
 		this.pos = pos;
 		this.direction = dir;
+		this.FOV = FOV;
+
+		// TODO: this.rotationMatrix = ...
+	}
+
+	iterateDirectionVectors (width, height, fn) {
+		const xViewArea = Math.tan(this.FOV);
+		const yViewArea = xViewArea * (height / width);
+		for (let x = 0; x < width; x++) {
+			for (let y = 0; y < height; y++) {
+				const xShift = (x / width - 0.5) * xViewArea;
+				const yShift = (y / height - 0.5) * yViewArea;
+
+				// TODO: don't just add, mul by rot matrix instead
+				// What we are currently doing creates weird stretches on the edges
+				const dir = new Vector(0, xShift, yShift).add(this.direction);
+				fn(x, y, dir);
+			}
+		}
 	}
 }
--- js/rayTracing.js
+++ js/rayTracing.js
@@ -1,17 +1,9 @@
-// rayTracing.js will be the entrypoint to the raytracing code. It will be separate from canvasRenderer.js as that will allow it to be used with ascii renderers etc.
+// rayTracing.js is the entrypoint to the raytracing code. It will be separate from canvasRenderer.js as that will allow it to be used with ascii renderers etc.
 
 import { Scene } from './scene.js';
 import { SphereObject } from './sphereObject.js';
 import { Vector } from './vector.js';
 import { Camera } from './camera.js';
-
-function raycast (pos, dir, scene) {
-	for (const obj of scene.objects) {
-		const hit = obj.rayIntersection({ origin: pos, direction: dir });
-		if (hit) return hit;
-	}
-	return null;
-}
 
 export class RayTracing {
 	constructor (buffer, width, height) {
@@ -22,29 +14,45 @@ export class RayTracing {
 		// create our scene
 		this.createScene();
 
-		// DEBUG: does our camera ray intersect the sphere:
-		for (let x = 0; x < width; x++) {
-			for (let y = 0; y < height; y++) {
-				const cameraPos = new Vector(0, x - width / 2, y - height / 2);
+		// render:
+		this.renderScene();
+	}
 
-				const hit = raycast(cameraPos, new Vector(1, 0, 0), this.scene);
-				if (hit) {
-					// const dist = (hit.hits[0].magnitude - 70) / 65;
-					const distanceToSphereCenter = hit.object.pos.sub(cameraPos).magnitude;
-					const brightness = Math.min((hit.hits[0].distance - (distanceToSphereCenter - hit.object.radius)) / (hit.object.radius), 1);
-					const a = 0xff - Math.ceil(0xff * brightness);
-					this.buffer[y * width + x] = 0xff000000 + (a << 16) + (a << 8) + a;
-				} else { this.buffer[y * width + x] = 0xff000000; }
+	raycast (origin, direction) {
+		let closest = null;
+		for (const obj of this.scene.objects) {
+			const hit = obj.rayIntersection({ origin: origin, direction: direction });
+			if (hit && (closest === null || hit.hits[0].distance < closest.hits[0].distance)) {
+				closest = hit;
 			}
 		}
+		return closest;
+	}
+
+	renderScene () {
+		const cameraPos = this.camera.pos;
+		this.camera.iterateDirectionVectors(this.width, this.height, (x, y, dir) => {
+			const hit = this.raycast(cameraPos, dir);
+
+			// DEBUG code for colouring the spheres. This is wrong, we should be treating the camera as a light source and using distance etc etc:
+			if (hit) {
+				this.buffer[y * this.width + x] = 0xffffffff;
+				const distanceToSphereCenter = hit.object.pos.sub(cameraPos).magnitude;
+				const brightness = Math.min((hit.hits[0].distance - (distanceToSphereCenter - hit.object.radius)) / (hit.object.radius), 1);
+				const a = 0xff - Math.ceil(0xff * brightness);
+				this.buffer[y * this.width + x] = 0xff000000 + (a << 16) + (a << 8) + a;
+			} else { this.buffer[y * this.width + x] = 0xff000000; }
+		});
 	}
 
 	createScene () {
 		this.scene = new Scene();
-		const sphere1 = new SphereObject(new Vector(100, -7, 0), 20);
-		const sphere2 = new SphereObject(new Vector(200, 10, 0), 15);
+		const sphere1 = new SphereObject(new Vector(2, 0, 0), 0.5);
+		const sphere2 = new SphereObject(new Vector(3, 0.7, 0), 0.5);
 		this.scene.addObject(sphere1);
 		this.scene.addObject(sphere2);
-		this.camera = new Camera(Vector.zero, new Vector(1, 0, 0));
+		const FOV = (60 / 360) * 2 * Math.PI;
+		// TODO: init camera with x and y rotations that it can use to form its rotation matrix
+		this.camera = new Camera(Vector.zero, new Vector(1, 0, 0), FOV);
 	}
 }

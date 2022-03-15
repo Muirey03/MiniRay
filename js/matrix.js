23306450eea8929dccc7adee8c084e6117f14996
Muirey03 <tcmuir03@gmail.com>
2022-03-15 21:27:39 +0000

    ESLint matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,10 +1,10 @@
-import { Vector } from "./vector.js";
+import { Vector } from './vector.js';
 
 export class Matrix {
-	constructor (col1 = new Vector(0, 0, 0), col2 = new Vector(0, 0, 0), col3 = new Vector(0, 0, 0)) {
-        this.col1 = col1;
-        this.col2 = col2;
-        this.col3 = col3;
+	constructor (col1 = Vector.zero, col2 = Vector.zero, col3 = Vector.zero) {
+		this.col1 = col1;
+		this.col2 = col2;
+		this.col3 = col3;
 	}
 
 	add (other) {
@@ -15,12 +15,11 @@ export class Matrix {
 		return new Vector(this.col1.sub(other.col1), this.col2.sub(other.col2), this.col3.sub(other.col3));
 	}
 
-	vect_mul (vect) {
-	    return this.col1.mul(vect.x).add(this.col2.mul(vect.y)).add(this.col3.mul(vect.z))
+	vectMul (vect) {
+		return this.col1.mul(vect.x).add(this.col2.mul(vect.y)).add(this.col3.mul(vect.z));
 	}
 
-    scale_mul (scalar) {
-        return new Vector(this.col1.mul(scalar), this.col2.mul(scalar), this.col3.mul(scalar));
-    }
-
+	scaleMul (scalar) {
+		return new Vector(this.col1.mul(scalar), this.col2.mul(scalar), this.col3.mul(scalar));
+	}
 }

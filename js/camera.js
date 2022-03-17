00bd17a9b61c8da7feaab634c9bd56cfd1e200eb
Tommy Muir <tcmuir03@gmail.com>
2022-03-17 19:55:26 +0000

    bye bye DEBUG code

--- js/camera.js
+++ js/camera.js
@@ -9,19 +9,11 @@ export class Camera {
 	}
 
 	iterateDirectionVectors (width, height, fn) {
-		// DEBUG CODE: let prevDir = Vector.zero;
 		const viewWidth = 2 * Math.tan(this.FOV / 2) * this.projDist;
 		const viewHeight = viewWidth * height / width;
 		for (let x = 0; x < width; x++) {
 			for (let y = 0; y < height; y++) {
 				const dir = this.rotMatrix.vectMul(new Vector(1, (x / width - 0.5) * viewWidth, (y / height - 0.5) * viewHeight));
-				/* DEBUG CODE:
-				if (x === 0 || y === 0) {
-					const newDir = dir.mul(1 / dir.x);
-					console.log(newDir.distance(prevDir));
-					prevDir = newDir;
-				}
-				*/
 				fn(x, y, dir);
 			}
 		}

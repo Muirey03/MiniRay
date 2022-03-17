b673027517b5a47d60ce27feacfce689e55fd683
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-03-17 19:51:28 +0000

    Implement some complicated mathematical optimisations

--- js/camera.js
+++ js/camera.js
@@ -11,8 +11,7 @@ export class Camera {
 	iterateDirectionVectors (width, height, fn) {
 		// DEBUG CODE: let prevDir = Vector.zero;
 		const viewWidth = 2 * Math.tan(this.FOV / 2) * this.projDist;
-		const verticalFOV = 2 * Math.atan(Math.tan(this.FOV / 2) * height / width);
-		const viewHeight = 2 * Math.tan(verticalFOV / 2) * this.projDist;
+		const viewHeight = viewWidth * height / width;
 		for (let x = 0; x < width; x++) {
 			for (let y = 0; y < height; y++) {
 				const dir = this.rotMatrix.vectMul(new Vector(1, (x / width - 0.5) * viewWidth, (y / height - 0.5) * viewHeight));
